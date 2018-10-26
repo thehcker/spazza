cd482db86045b83b6ce6e0b1014200ae0a7d243f
thehcker <isaacnyakoi1@gmail.com>
2018-10-27 00:45:06 +0300

    Edit static settings

--- swahilibox/settings.py
+++ swahilibox/settings.py
@@ -141,10 +141,10 @@ USE_TZ = True
 STATIC_URL = '/static/'
 if DEBUG:
     STATIC_MEDIA = '/media/'
-    STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static","static-only")
-    MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static","media")
+    STATIC_ROOT = os.path.join(BASE_DIR, "static","static-only")
+    MEDIA_ROOT = os.path.join(BASE_DIR, "static","media")
     STATICFILES_DIRS = (
-            os.path.join(os.path.dirname(BASE_DIR), "static","static"),
+            os.path.join(BASE_DIR, "static","static"),
         )    
 
 CRISPY_TEMPLATE_PACK = 'bootstrap3'
